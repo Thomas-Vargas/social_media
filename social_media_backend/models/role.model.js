8e2929bdb3f4adae5b0655b1114101f0b17d6f24
Thomas Vargas <tavargas2012irc@gmail.com>
2022-05-04 14:18:20 -0500

    Initialized mongoose.

---
+++ social_media_backend/models/role.model.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+const mongoose = require('mongoose')
+
+const Role = mongoose.model(
+    'Role',
+    new mongoose.Schema({
+        name: String
+    })
+)
+
+module.exports = Role
--- social_media_backend/server.js
+++ social_media_backend/server.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-undef */
 const express = require('express')
 const cors = require('cors')
+const db = require('./app/models')
+const Role = db.role
 const app = express()
+
 var corsOptions = {
     origin: 'http://localhost:8081'
 }
@@ -13,6 +16,51 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the application.'})
 })
+
+db.mongoose
+    .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Successfully connect to MongoDB.')
+        initial()
+    })
+    .catch(err => {
+        console.error('Connection error', err)
+        process.exit()
+    })
+
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if(!err && count === 0) {
+            new Role({
+                name: 'user'
+            }).save(err => {
+                if(err) {
+                    console.log('error', err)
+                }
+                console.log('added "user" to roles collection')
+            })
+            new Role({
+                name: 'moderator'
+            }).save(err => {
+                if(err) {
+                    console.log('error', err)
+                }
+                console.log('added "moderator" to roles collection')
+            })
+            new Role({
+                name: 'admin'
+            }).save(err => {
+                if(err) {
+                    console.log('error', err)
+                }
+                console.log('added "admin" to roles collection')
+            })
+        }
+    })
+}
 
 const PORT = process.env.PORT || 8080
 
