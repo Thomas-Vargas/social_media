b58ae7cc5ee59c8ce1e37c40cb9bd62004e1d665
Thomas Vargas <tavargas2012irc@gmail.com>
2022-05-05 14:37:03 -0500

    refresh token model.

--- social_media_backend/middleware/authJwt.js
+++ social_media_backend/middleware/authJwt.js
@@ -18,7 +18,7 @@ const catchError = (err, res) => {
 const verifyToken = (req, res, next) => {
     let token = req.headers['x-access-token']
     if(!token) {
-        return res.status(403).send({ message: 'No token procided!' })
+        return res.status(403).send({ message: 'No token provided!' })
     }
     jwt.verify(token, config.secret, (err, decoded) => {
         if(err) {
