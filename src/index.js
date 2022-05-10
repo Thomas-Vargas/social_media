878552183e2f9845e260dfe71d36a8fdd549a1ed
Thomas Vargas <tavargas2012irc@gmail.com>
2022-05-10 14:05:07 -0500

    Test pages and components, renders TestAPP

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import '../node_modules/font-awesome/css/font-awesome.min.css'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+//import App from './App'
+import TestApp from './TestApp'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <TestApp />
         </BrowserRouter>
     </React.StrictMode>
 )
